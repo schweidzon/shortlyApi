0edd657f05f06fcc06db6995980f44a08f397193
Schweidzon <dannielsch@gmail.com>
2023-02-27 15:05:28 -0300

    fix: adicionando coluna createdAt ao signUp

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -4,10 +4,11 @@ import bcrypt from 'bcrypt'
 export async function signUp(req, res) {
     const user = req.body
     const encryptPassword = bcrypt.hashSync(user.password, 10)
+    
 
     try {
         await db.query(`INSERT INTO users (name, email, password) VALUES ('${user.name}', '${user.email}', '${encryptPassword}')`)
-        res.send('tudo ok')
+        res.sendStatus(201) 
     } catch (error) {
         console.log(error)
     }
