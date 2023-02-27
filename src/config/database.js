a752e278b312d18e96e6cb90f99efc53357aadf1
Schweidzon <dannielsch@gmail.com>
2023-02-27 19:03:33 -0300

    fix: .env

--- src/config/database.js
+++ src/config/database.js
@@ -6,10 +6,9 @@ const {Pool} = pg
 
 const configDatabase = {
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
 }
+
+if(process.env.MODE === "prod") configDatabase.ssl = true;
 
 const db = new Pool(configDatabase)
 export default db
