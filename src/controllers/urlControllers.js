b4ebd0157afdd2563cd991e6041def5501d318b2
Schweidzon <dannielsch@gmail.com>
2023-02-27 21:37:56 -0300

    feat: adicionando rota de links do usuário

--- src/controllers/urlControllers.js
+++ src/controllers/urlControllers.js
@@ -27,7 +27,7 @@ export async function shortenUrl(req, res) {
 
 export async function getUrlById(req, res) {
 
-    const {id} = req.params
+    const { id } = req.params
 
     try {
 
@@ -36,31 +36,55 @@ export async function getUrlById(req, res) {
         if (!shortUrl.rows[0]) return res.sendStatus(404)
 
         res.status(200).send(shortUrl.rows[0])
-        
+
     } catch (error) {
         console.log(error.message)
         return res.status(500).send(error.message)
-        
+
     }
 
 }
 
+export async function getUsersUrls(req, res) {
+
+    const token = res.locals.token
+
+    try {
+        const user = await db.query(`SELECT users.id, users.name
+         FROM sessions JOIN users ON sessions.user_id = users.id  JOIN urls ON user_token = $1 WHERE token = $1 GROUP BY users.id`, [token])
+        const usersUrls = await db.query(`SELECT json_agg(json_build_object('id', urls.id, 'shortUrl', urls."shortUrl", 'url', urls.url, 'visitCount', urls."visitCount")) AS "shortenedUrls"
+        FROM urls
+        WHERE user_token = $1`, [token])
+
+        const totalVistCount = (usersUrls.rows[0].shortenedUrls).reduce((acc, curr) => acc + parseInt(curr.visitCount), 0)
+        user.rows[0].visitCount = totalVistCount
+        user.rows[0].shortenedUrls = usersUrls.rows[0].shortenedUrls
+
+        res.status(200).send(user.rows[0])
+
+    } catch (error) {
+        console.log(error.message)
+        return res.status(500).send(error.message)
+
+    }
+}
+
 export async function redirectToUrl(req, res) {
-    const {shortUrl} = req.params
+    const { shortUrl } = req.params
 
     try {
 
-        const url =await db.query(`SELECT * FROM urls where "shortUrl" = $1`,[shortUrl])
-        
-        if(!url.rows[0]) return res.sendStats(404)
-        
-        await db.query(`UPDATE urls SET "visitCount" = "visitCount" + 1 WHERE id = $1`,[url.rows[0].id])
-       
+        const url = await db.query(`SELECT * FROM urls where "shortUrl" = $1`, [shortUrl])
+
+        if (!url.rows[0]) return res.sendStatus(404)
+
+        await db.query(`UPDATE urls SET "visitCount" = "visitCount" + 1 WHERE id = $1`, [url.rows[0].id])
+
 
         res.redirect(url.rows[0].url)
     } catch (error) {
         console.log(error.message)
         return res.status(500).send(error.message)
-        
+
     }
 }
--- src/routes/urlsRoutes.js
+++ src/routes/urlsRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getUrlById, redirectToUrl, shortenUrl } from "../controllers/urlControllers.js";
+import { getUrlById, getUsersUrls, redirectToUrl, shortenUrl } from "../controllers/urlControllers.js";
 import { authValidation } from "../middlewares/authMid.js";
 import { validateSchema } from "../middlewares/validateSchemas.js";
 import { shortenUrlSchema } from "../schemas/urlSchemas.js";
@@ -8,6 +8,7 @@ const urlRoutes = Router()
 
 urlRoutes.post("/urls/shorten", validateSchema(shortenUrlSchema), authValidation, shortenUrl)
 urlRoutes.get("/urls/:id", getUrlById)
+urlRoutes.get("/users/me", authValidation, getUsersUrls)
 urlRoutes.get("/urls/open/:shortUrl", redirectToUrl)
 
 export default urlRoutes
